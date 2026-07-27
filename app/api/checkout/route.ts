9c73f14f76f75da78912e59917df67a79a2509fa
Thang <thang.edreams@gmail.com>
2026-07-27 15:41:02 +0700

    allow promo code on stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -78,6 +78,7 @@ export async function POST(request: Request) {
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
       payment_method_types: ['card'],
+      allow_promotion_codes: true,
       line_items: [{ price: priceId, quantity: 1 }],
       customer: stripeCustomerId,
       metadata: {
